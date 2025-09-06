a357de5e46465583b403a0477252f0550c4d70a8
Craig Sampson <craigsjr15@gmail.com>
2025-09-05 21:28:26 -0400

    modals updated for admin page

--- app/components/Modals/ClientProjectModal.tsx
+++ app/components/Modals/ClientProjectModal.tsx
@@ -4,10 +4,16 @@ export default function ClientProjectModal({
   onClose: () => void;
 }) {
   return (
-    <div className="z-50 fixed bottom-20 bg-gray bg-opacity-50 flex items-center justify-center h-3/4 w-7/8 shadow-lg">
+    <div className="z-50 fixed bottom-20 bg-gradient-to-l from-primary to-secondary bg-opacity-50 border-2 rounded flex items-center justify-center h-3/4 w-7/8 shadow-lg">
       <div className="flex flex-col items-center gap-5">
         <h2 className="text-2xl font-jetbrains font-bold text-dark">Add Client Project</h2>
-        <button className="bg-dark rounded-lg px-4 py-2 text-white" onClick={onClose}>Close</button>
+
+        <form className="flex flex-col gap-4 w-3/4">
+        
+        </form>
+          
+
+        <button className="button py-1 px-4 bg-dark text-white hover:bg-dark-accent border-dark" onClick={onClose}>Close</button>
       </div>
     </div>
   );
