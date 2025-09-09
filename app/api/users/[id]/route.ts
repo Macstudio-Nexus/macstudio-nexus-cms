d8d5e63e1794d5bee2fae6bf5eb6968252c9afea
Craig Sampson <craigsjr15@gmail.com>
2025-09-09 17:31:31 -0400

    api route folder structure fixed

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,19 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../lib/prisma";
 
-export async function GET() {
+
+// Get a user by id
+export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   try {
-    const users = await prisma.users.findMany({
-      orderBy: { id: "asc" }, // optional, orders by id
+    const id = parseInt((await params).id, 10);
+
+    if (isNaN(id)) {
+      return NextResponse.json({ error: "Invalid user id" }, { status: 400 });
+    }
+
+    const user = await prisma.users.findUnique({
+      where: { id },
     });
 
-    return NextResponse.json({ users });
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ user });
   } catch (error) {
-    console.error("Error fetching users:", error);
-    return NextResponse.json({ error: "Could not fetch users" }, { status: 500 });
+    console.error("Error fetching user:", error);
+    return NextResponse.json({ error: "Could not fetch user" }, { status: 500 });
   }
 }
 
+// Delete a user by id
 export async function DELETE(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   try {
     const id = parseInt((await params).id, 10);
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../lib/prisma";
 import bcrypt from "bcrypt";
 
+// Create a new user
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
@@ -24,5 +25,19 @@ export async function POST(req: NextRequest) {
     const errorMessage =
       error instanceof Error ? error.message : "An unknown error occurred";
     return NextResponse.json({ error: errorMessage }, { status: 500 });
+  }
+}
+
+// Get all users
+export async function GET() {
+  try {
+    const users = await prisma.users.findMany({
+      orderBy: { id: "asc" }, // optional, orders by id
+    });
+
+    return NextResponse.json({ users });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return NextResponse.json({ error: "Could not fetch users" }, { status: 500 });
   }
 }
